cb6bcab02241e07a5c01f676a4cf3f66e9c564ec
Dominic Lapointe <dom.dlapointe@gmail.com>
2014-04-29 16:52:56 -0400

    Fix 401 error json

--- lib/failure.rb
+++ lib/failure.rb
@@ -2,6 +2,6 @@ class Failure < Devise::FailureApp
     def respond
         self.status = 401
         self.content_type = 'json'
-        self.response_body = '{"error" : "authentication error"}'
+        self.response_body = '{ "success":"false", "info":"Access denied" }'
     end
 end
