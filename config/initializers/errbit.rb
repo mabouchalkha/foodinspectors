ef11f775330111a3bd5c02b93b931e5c228c05b7
Dominic Lapointe <dom.dlapointe@gmail.com>
2014-04-29 17:25:36 -0400

    Fix errbit with new app

--- config/initializers/errbit.rb
+++ config/initializers/errbit.rb
@@ -1,7 +1,7 @@
 Airbrake.configure do |config|
   config.ignore_only = []
   config.development_environments = []
-  config.api_key = 'dd26695851224c0adbdae34f5471093a'
+  config.api_key = '0739dd9c53c8b879dcfc909bd9d3bbfd'
   config.host    = 'errbitrails.herokuapp.com'
   config.port    = 80
   config.secure  = config.port == 443
