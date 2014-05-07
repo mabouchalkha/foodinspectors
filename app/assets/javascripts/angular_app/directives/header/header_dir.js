55491dce6ba73040bb148026c520a42ea2d9ebba
Dominic Lapointe <dom.dlapointe@gmail.com>
2014-05-07 08:51:59 -0400

    Finish changing template (from neon to ace admn)

--- app/assets/javascripts/angular_app/directives/header/header_dir.js
+++ app/assets/javascripts/angular_app/directives/header/header_dir.js
@@ -1,11 +1,19 @@
-angular.module("starterApp").directive('header', function () {
+angular.module("starterApp").directive('header', ['Session', function (Session) {
     return {
         restrict: 'E',
         templateUrl: 'angular_app/directives/header/header.html',
         replace: true,
         transclude: true,
-        scope: {
+        link: function($scope, element, attrs) {
+            var _init = function () {
+                
+            };
             
+            $scope.logout = function () {
+                Session.logout();
+            };
+            
+            _init();
         }
     };
-});
+}]);
