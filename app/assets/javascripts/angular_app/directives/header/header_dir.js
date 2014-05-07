94d800b6791f5c34e1e49a48923915e657bfa6cd
Dominic Lapointe <dom.dlapointe@gmail.com>
2014-05-07 12:42:41 -0400

    Fix minor bug to sidebar/header and add user name to header

--- app/assets/javascripts/angular_app/directives/header/header_dir.js
+++ app/assets/javascripts/angular_app/directives/header/header_dir.js
@@ -6,8 +6,24 @@ angular.module("starterApp").directive('header', ['Session', function (Session) 
         transclude: true,
         link: function($scope, element, attrs) {
             var _init = function () {
-                
+                $scope.user = {};
+                $scope.service = Session;
             };
+            
+            $scope.$watch('service.currentUser', function(user, oldUser) {
+                if(!_.isEmpty(user))
+                    if (!_.isEmpty(user.first_name) && !_.isEmpty(user.last_name)) {
+                        $scope.user.name = user.first_name + ' ' + user.last_name;   
+                    }
+                    else if (!_.isEmpty(user.user_name)) {
+                        $scope.user.name = user.user_name
+                    }
+                    else {
+                        $scope.user.name = user.email
+                    }
+                else
+                    $scope.user = {};
+            });
             
             $scope.logout = function () {
                 Session.logout();
