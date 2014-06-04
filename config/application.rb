df6662a16be0f73613e00cb84215d4b6f6c2a15b
Dominic Lapointe <dom.dlapointe@gmail.com>
2014-06-04 09:47:34 -0400

    Finish test for payement_term

--- config/application.rb
+++ config/application.rb
@@ -26,7 +26,7 @@ module PlaygroundOnrails
         g.test_framework :rspec, 
             :fixtures => true, 
             :view_specs => false, 
-            :helper_specs => false, 
+            :helper_specs => true, 
             :routing_specs => true, 
             :controller_specs => true, 
             :request_specs => true 
