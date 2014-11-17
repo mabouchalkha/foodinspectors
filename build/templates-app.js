e740c544442a18fbab7073c4eacf7f2664e9fe1b
Mohamed amine bouchalkha <mabouchalkha@gmail.com>
2014-11-16 21:47:12 -0500

    not necessary to add vendor and build directories so add it to ignore git

--- build/templates-app.js
+++
@@ -1,8 +0,0 @@
-angular.module('templates-app', ['login/login.tpl.html']);
-
-angular.module("login/login.tpl.html", []).run(["$templateCache", function($templateCache) {
-  $templateCache.put("login/login.tpl.html",
-    "<div>\n" +
-    "	This will be a login form!\n" +
-    "</div>");
-}]);
