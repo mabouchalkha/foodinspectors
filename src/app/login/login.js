c3f3a94ee6ca5a25e7eebea54cca6c851d637b1b
mohamed  amine <mabouchalkha@gmail.com>
2014-11-18 17:31:34 +0000

    add template html in progress

--- src/app/login/login.js
+++ src/app/login/login.js
@@ -1,11 +1,11 @@
 angular.module('inspectors.login', [
-	'ui.router'])
-	.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
-		function ($stateProvider, $urlRouterProvider, $locationProvider) {
-		   $stateProvider
-		      .state('login', {
-		        url: '/login',
-		        templateUrl: 'login/login.tpl.html'
-	      	});
-	}])
-	.value('test', 2);
+		'ui.router'])
+			.config(['$stateProvider', '$urlRouterProvider', '$locationProvider', 
+				function ($stateProvider, $urlRouterProvider, $locationProvider) {
+				   $stateProvider
+				      .state('login', {
+				        url: '/login',
+				        templateUrl: 'login/login.tpl.html'
+			      	});
+			}])
+			.value('test', 2);
