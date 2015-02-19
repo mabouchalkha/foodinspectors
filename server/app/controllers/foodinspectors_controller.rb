08af726d93bcad73402059088b9a46f2ca8b6ad8
Mohamed amine bouchalkha <mabouchalkha@gmail.com>
2015-02-19 08:08:36 -0500

    move backend to an other repo

--- server/app/controllers/foodinspectors_controller.rb
+++
@@ -1,5 +0,0 @@
-class PopcornController < ApplicationController
-  def index
-    
-  end
-end
--- server/config/initializers/i18n.rb
+++
@@ -1,6 +0,0 @@
-I18n.default_locale = :en
-
-LANGUAGES = [
-  ['English', 'en'],
-  ["Francais", 'fr']
-]
